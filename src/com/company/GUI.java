8f6ca19593050242aba203c987761da1c6f1d703
ahat92 <102114179+ahat92@users.noreply.github.com>
2022-03-28 08:40:31 -0400

    Update GUI.java

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -8,7 +8,8 @@ public class GUI {
     public static JPanel startScreen, gameScreen, endScreen;
     public static Color white = new Color(248,240,227);
     public static Color black = new Color(20,20,20);
-    public static Color background = new Color(104,31,0);
+    public static Color background = new Color(93, 30, 3);
+    public static Color background2 = new Color(164, 104, 55);
     public GUI(){
         frame= new JFrame("chess");
         startScreen = new StartScreen();
@@ -19,8 +20,8 @@ public class GUI {
         frame.setResizable(false);
         frame.setVisible(true);
     }
-    public static void swapScreen(JPanel next){
-        frame.remove();
+    public static void swapScreen(JPanel current, JPanel next){
+        frame.remove(current);
         frame.add(next);
         next.updateUI();
     }
