cf335e6bfad66455a5cdc6f07c704f978d5d6e05
ahat92 <102114179+ahat92@users.noreply.github.com>
2022-03-30 08:49:22 -0400

    Add files via upload

--- src/com/company/GameScreen.java
+++ src/com/company/GameScreen.java
@@ -2,17 +2,17 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class GameScreen extends JPanel{
 
-    JPanel playBoard, title;
+    JPanel playBoard;
     Board board;
+    Piece selected;
     public GameScreen(){
         board = new Board();
+        board.printBoardState();
 
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -24,15 +24,15 @@ public class GameScreen extends JPanel{
         c.gridx = 0;
         c.gridy = 0;
         c.ipadx = 1280;
-        c.ipady = 670;
+        c.ipady = 675;
         this.add(playBoard,c);
 
     }
 
     public class chessBoard extends JPanel implements MouseListener {
-        int startx = 150;
-        int starty = 40;
-        int width = 50;
+        int startX = 40;
+        int startY = 41;
+        int width = 75;
         public chessBoard(){
             addMouseListener(this);
         }
@@ -43,29 +43,37 @@ public class GameScreen extends JPanel{
             for (int i = 0; i < 8; i++) {
                 color = !color;
                 for (int j = 0; j < 8; j++) {
-                    if (color) {
+                    if(color) {
                         g.setColor(Color.black);
-                        color = !color;
+                        color = false;
                     } else {
                         g.setColor(Color.WHITE);
-                        color = !color;
+                        color = true;
                     }
-                    g.fillRect(startx + (i * width), starty + (j * width), width, width);
+                    g.fillRect(startX + (i * width), startY + (j * width), width, width);
+
                 }
             }
+            if(selected != null){
+                g.setColor(Color.CYAN);
+                g.drawRect(startX + (selected.getRow() * width), startY + (selected.getColumn() * width),width,width);
+            }
         }
-        public void posClicked(int x, int y){
-
+        public int[] posClicked(int x, int y){
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-                    if(x == startx +(i * width) && y == starty + (j * width)){
-
+                    if(startX +(i * width) <= x && x < startX +((i+1) * width) && startY +(j * width) <= y && y < startY +((j+1) * width)){
+                        System.out.println((i+1) + " " + (j+1));
+                        return new int[]{i, j};
                     }
                 }
             }
+            return new int[]{-1};
         }
         public void mouseClicked(MouseEvent e) {
-            posClicked(e.getX(), e.getY());
+            int[] index = posClicked(e.getX(), e.getY());
+            selected = board.getBoard()[index[0]][index[1]];
+            this.repaint();
         }
         public void mousePressed(MouseEvent e) {}
         public void mouseReleased(MouseEvent e) {}
