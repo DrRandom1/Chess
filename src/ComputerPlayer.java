7fe2baa8c321ce0eb7aac74e4c03eead05cd404a
DrRandom1 <49456942+DrRandom1@users.noreply.github.com>
2022-05-12 08:48:56 -0400

    Update ComputerPlayer.java

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 public class ComputerPlayer extends Player{
@@ -8,15 +11,90 @@ public class ComputerPlayer extends Player{
         isComputerPlayer=true;
     }
 
-    public Move getMove() {
+    public Move getRandomMove() {
         Random random=new Random();
         int randomPiece=random.nextInt(getMovablePieces().size());
         Piece piece =getMovablePieces().get(randomPiece);
         return piece.getMoves().get(random.nextInt(piece.getMoves().size()));
     }
+    public Move findBestMove(int depth){
+        Move bestMove=null;
+        if (depth==0){
+            bestMove=getBestMove();
+        } else{
+            HashMap<Move, Double> moves=new HashMap<>();
+            for (Piece piece: board.getPiecesOnBoard(this.getColor())) {
+                for (Move move : piece.getMoves()) {
+                    moves.put(move,evaluatePosition(new Board(board,move)));
+                }
+            }
+            bestMove=findBestMove(depth-1);
+        }
+        return bestMove;
+    }
+    public Move getBestMove() {
+        HashMap<Move, Double> moves=new HashMap<>();
+        for (Piece piece: board.getPiecesOnBoard(this.getColor())) {
+            for (Move move : piece.getMoves()) {
+                moves.put(move,evaluatePosition(new Board(board,move)));
+            }
+        }
+        Move bestMove=null;
+        switch(this.getColor()){
+            case('w')->{
+                double best=-1000;
+                for (Move move : moves.keySet()) {
+                    if(moves.get(move)>best){
+                        bestMove=move;
+                        best=moves.get(bestMove);
+                    }
+                }
+            }
+            case('b')->{
+                double best=1000;
+                for (Move move : moves.keySet()) {
+                    if(moves.get(move)<best){
+                        bestMove=move;
+                        best=moves.get(bestMove);
+                    }
+                }
+            }
+        }
+        return bestMove;
+    }
+    public Move getOppBestMove() {
+        HashMap<Move, Double> moves=new HashMap<>();
+        for (Piece piece: board.getPiecesOnBoard(this.getColor())) {
+            for (Move move : piece.getMoves()) {
+                moves.put(move,evaluatePosition(new Board(board,move)));
+            }
+        }
+        Move bestMove=null;
+        switch(this.getColor()){
+            case('b')->{
+                double best=-1000;
+                for (Move move : moves.keySet()) {
+                    if(moves.get(move)>best){
+                        bestMove=move;
+                        best=moves.get(bestMove);
+                    }
+                }
+            }
+            case('w')->{
+                double best=1000;
+                for (Move move : moves.keySet()) {
+                    if(moves.get(move)<best){
+                        bestMove=move;
+                        best=moves.get(bestMove);
+                    }
+                }
+            }
+        }
+        return bestMove;
+    }
     public static double evaluatePosition(Board board){
         double eval=0;
-        for (Piece piece : board.getPieces()) {
+        for (Piece piece : board.getPiecesOnBoard()) {
             //base piece value
             int val=piece.getColor()=='w'?1:-1;
             switch (piece.getName()){
