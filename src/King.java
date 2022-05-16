934732050827d32b943b8954dabf5417d4c68a9d
DrRandom1 <49456942+DrRandom1@users.noreply.github.com>
2022-05-16 09:02:03 -0400

    Update King.java

--- src/King.java
+++ src/King.java
@@ -57,10 +57,11 @@ public class King extends Piece{
         for (int i = 0; i < 4; i++) {
             int[] key=Bishop.key[i];
             int counter=1;
-            //BUGGGGGUGUGUGUGUG
             while(row+counter*key[0]>=0&&row+counter*key[0]<8&&column+counter*key[1]>=0&&column+counter*key[1]<8
                     &&(board.getPiece(row+counter*key[0],column+counter*key[1])==null
-                    ||board.getPiece(row+counter*key[0],column+counter*key[1]).getColor()!=this.getColor())) {
+                    ||(board.getPiece(row+counter*key[0],column+counter*key[1]).getColor()!=this.getColor()
+                    &&(board.getPiece(row+counter*key[0],column+counter*key[1]).getName()=='b')
+                    ||board.getPiece(row+counter*key[0],column+counter*key[1]).getName()=='q'))) {
                 if(board.getPiece(row+counter*key[0],column+counter*key[1])!=null
                         &&(board.getPiece(row+counter*key[0],column+counter*key[1]).getName()=='b'
                         ||board.getPiece(row+counter*key[0],column+counter*key[1]).getName()=='q')){
